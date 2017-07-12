fe2a9b0a93d27fb0be0a45709159beddadee1beb
tianzx <tianzx@aliyun.com>
2017-07-12 09:29:05 +0800

    go on finish mock memcache client

---
+++ client/demo/mem_04/index.js
@@ -0,0 +1,42 @@
+/**
+ * Created by tianzx on 2017/7/12.
+ */
+const net = require("net");
+
+Client = exports.Client = function (port, host) {
+    this.port = port
+    this.host = host
+    this.conn = null
+}
+
+Client.prototype.connect = function () {
+    this.conn = net.connect({port: this.port, host: this.host}, () => {
+        console.log('connected');
+        /**
+         * memcache set function
+         */
+        this.conn.write('set key 0 0 6 \r\n')
+        this.conn.write('tianzx\r\n')
+        /**
+         * memcache get function
+         */
+        // client.write('get key \r\n');
+        //OR other commands + "\r\n"
+
+        this.conn.on('data', function (data) {
+            console.log('-----data begin------')
+            console.log(data.toString());
+            console.log('-----data end------')
+        });
+
+        this.conn.on('end', function () {
+            console.log('-----end------')
+            console.log('data fetched');
+        });
+    })
+}
+
+Client.prototype.getKey = function (key, callback) {
+    this.conn.write('get ' + key + '\r\n');
+    // return this.query('get ' + key, 'get', callback);
+};
