79cb50e99ccd2b245f4e95ffcc52b930d98196a4
tianzx <tianzx@aliyun.com>
2017-07-12 09:29:42 +0800

    change package directory

--- client/node/demo/index.js
+++ client/node/demo/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by tianzx on 2017/7/11.
  */
-const Client = require('./mem_03').Client
+const Client = require('./mem_03/index').Client
 
 const client  =  new Client(17465,'memcached-17465.c1.asia-northeast1-1.gce.cloud.redislabs.com')
 
